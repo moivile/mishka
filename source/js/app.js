2b9200eb50865bf58342b44488af4ed4e6095d66
Victor Moiseiev <vimo@247e.com>
2019-01-23 13:37:22 +0200

    Create contacts__map iframe, contacts__map--image

--- source/js/app.js
+++ source/js/app.js
@@ -1,18 +1,22 @@
-  var navMain = document.querySelector(".main-nav");
-  var navToggle = document.querySelector(".main-nav__menu-toggle");
+document.querySelector(".contacts").classList.remove("contacts--nojs");
 
-  navMain.classList.remove("main-nav--nojs");
+var navMain = document.querySelector(".main-nav");
+navMain.classList.remove("main-nav--nojs");
 
-  if (!navMain.classList.contains("main-nav--closed") && !navMain.classList.contains("main-nav--opened")) {
+var navToggle = document.querySelector(".main-nav__menu-toggle");
+
+
+
+if (!navMain.classList.contains("main-nav--closed") && !navMain.classList.contains("main-nav--opened")) {
+  navMain.classList.add("main-nav--closed");
+}
+
+navToggle.addEventListener("click", function () {
+  if (navMain.classList.contains("main-nav--closed")) {
+    navMain.classList.remove("main-nav--closed");
+    navMain.classList.add("main-nav--opened");
+  } else {
     navMain.classList.add("main-nav--closed");
+    navMain.classList.remove("main-nav--opened");
   }
-
-  navToggle.addEventListener("click", function () {
-    if (navMain.classList.contains("main-nav--closed")) {
-      navMain.classList.remove("main-nav--closed");
-      navMain.classList.add("main-nav--opened");
-    } else {
-      navMain.classList.add("main-nav--closed");
-      navMain.classList.remove("main-nav--opened");
-    }
-  });
+});
