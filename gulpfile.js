3aac7f5d4c51166b458b711aa2431b193cfc34fd
Victor Moiseiev <vimo@247e.com>
2019-03-27 12:49:35 +0200

    Minify css with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -34,6 +34,9 @@ gulp.task("css", function () {
       autoprefixer()
     ]))
     .pipe(gulp.dest("build/css"))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
